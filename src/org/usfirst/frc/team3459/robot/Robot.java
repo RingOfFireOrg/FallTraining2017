488c15d1c848ccd2485055c0a6466cfcd0862d7e
Alan Smith <alan@randomsmiths.com>
2017-09-14 13:30:30 -0400

    simplest possible do nothing robot

---
+++ src/org/usfirst/frc/team3459/robot/Robot.java
@@ -0,0 +1,59 @@
+package org.usfirst.frc.team3459.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * Don't change the name of this or it won't work. (The manifest looks for
+ * "Robot")
+ */
+public class Robot extends IterativeRobot {
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+	}
+
+	/**
+	 * This function is called once when we go into the autonomous mode
+	 */
+	@Override
+	public void autonomousInit() {
+	}
+
+	/**
+	 * This function is called periodically during autonomous (approx 20ms)
+	 */
+	@Override
+	public void autonomousPeriodic() {
+	}
+
+	/**
+	 * This function is called once when we go into the teleop mode
+	 */
+	@Override
+	public void teleopInit() {
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+	}
+
+	/**
+	 * This function is called once when we go into the test mode
+	 */
+	@Override
+	public void testInit() {
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+}
